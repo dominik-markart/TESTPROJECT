ee4012c64d1d53755ea8a1320d7c14e13980bebe
Dominik Markart <dominik.markart@cefriel.com>
2022-09-08 13:55:45 +0200

    ...

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -26,7 +26,7 @@ public class TestTest {
   JavascriptExecutor js;
   @BeforeEach
   public void setUp() {
-    System.setProperty("webdriver.chrome.driver","/var/jenkins_home/tools/chromedriver");
+    System.setProperty("webdriver.chrome.driver","/var/jenkins_home/tools/chromedriver/chromedriver.exe");
     driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
